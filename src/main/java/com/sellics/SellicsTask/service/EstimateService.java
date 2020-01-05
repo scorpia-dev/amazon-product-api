5be6b998b4e9f08e24672dea7c85f9e54aff3acb
nick prendergast <nick_prendergast@hotmail.com>
2020-01-05 12:44:08 +0000

    validation

--- src/main/java/com/sellics/SellicsTask/service/EstimateService.java
+++ src/main/java/com/sellics/SellicsTask/service/EstimateService.java
@@ -9,6 +9,9 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.commons.lang3.StringUtils;
+
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -28,27 +31,36 @@ public class EstimateService {
 		long startTime = System.currentTimeMillis();
 		int finalScore = 0;
 		int score = 0;
+		
+		if (isKeyWordValidInput(keyWord)) {
 
-		for (int i = 0; i < keyWord.length(); i++) {
+			for (int i = 0; i < keyWord.length(); i++) {
 
-			String keyWordSubString = keyWord.substring(0, i + 1);
-			
-			if (underTenSeconds(startTime)) {
-				
-				JsonArray subStringJsonArray = getProductList(keyWordSubString);
-				Set<String> subStringList = convertToArrayList(subStringJsonArray);
-				score = getSubStringScore(subStringList, keyWord) + score;
-			
-			} else {
-				System.out.println("microservice only has an SLA of 10 seconds for a request round-trip​.");
-				break;
+				String keyWordSubString = keyWord.substring(0, i + 1);
+
+				if (underTenSeconds(startTime)) {
+
+					JsonArray subStringJsonArray = getProductList(keyWordSubString);
+					Set<String> subStringList = convertToArrayList(subStringJsonArray);
+					score = getSubStringScore(subStringList, keyWord) + score;
+
+				} else {
+					throw new RuntimeException("microservice only has an SLA of 10 seconds for a request round-trip​.");
+				}
 			}
+		} else {
+			throw new RuntimeException("Invalid input, key word must start with Alpha numeric character");
 		}
 
 		finalScore = getFinalScore(keyWord, score);
-
+		
 		return new Estimate(keyWord, finalScore);
 
+	}
+
+	private boolean isKeyWordValidInput(String keyWord) {
+		String firstChar = keyWord.substring(0, 1); 
+		return (StringUtils.isAlphanumeric(firstChar));
 	}
 
 	private int getFinalScore(String keyWord, int score) {
@@ -65,7 +77,7 @@ public class EstimateService {
 		JsonParser jp = new JsonParser();
 
 		JsonElement jsonElement = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-		 return jsonElement.getAsJsonArray();
+		return jsonElement.getAsJsonArray();
 	}
 
 	private boolean underTenSeconds(Long startTime) {
@@ -85,9 +97,11 @@ public class EstimateService {
 	private Set<String> convertToArrayList(JsonArray jsonArray) {
 		Set<String> subStringList = new HashSet<String>();
 		if (jsonArray != null) {
-			TypeToken<ArrayList<String>> token = new TypeToken<ArrayList<String>>() {
-			};
+			
+			TypeToken<ArrayList<String>> token = new TypeToken<ArrayList<String>>() {};
+			
 			ArrayList<String> productList = new Gson().fromJson(jsonArray.get(1), token.getType());
+			
 			for (int i = 0; i < productList.size(); i++) {
 				subStringList.add(productList.get(i));
 			}
